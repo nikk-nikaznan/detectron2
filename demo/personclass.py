3fe07b2ab017aa7bb977ba2111c3c205e16b1fe4
Nik Khadijah <nik.k.nik-aznan@durham.ac.uk>
2021-01-09 18:11:11 +0000

    save results as kitti format

--- demo/personclass.py
+++ demo/personclass.py
@@ -1,6 +1,7 @@
 import detectron2
 import numpy as np
 import os, cv2
+import sys
 
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
@@ -9,8 +10,9 @@ from detectron2.utils.visualizer import Visualizer
 
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
-filename = "TFL-DK38-Vic-616C-D040-----02-07h38m30s-19.06.2020.avi"
-vid_path = "/home/nikkhadijah/Data/Vic-On-Train-CCTV/"+filename
+filename = "video.avi"
+kitti_filename = "Export_video_Detectron2_results.txt"
+vid_path = "/home/"+filename
 video = cv2.VideoCapture(vid_path)
 width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -47,41 +49,46 @@ while(video.isOpened()):
         instances = panoptic["instances"]
         category_detections = instances[instances.pred_classes == 0]
         confident_detections = category_detections[category_detections.scores > 0.5]
-
-        # print (confident_detections.pred_classes)
-        print (len(confident_detections.pred_classes))
-        print (confident_detections.pred_boxes)
-        output_pred_boxes = confident_detections.pred_boxes
-        for i in output_pred_boxes.__iter__():
-            bbox = i.cpu().numpy()
-            print(bbox)
         
-        # instances.gt_boxes = confident_detections.pred_boxes[0]
-        # box1 = confident_detections.pred_boxes[0]
-        # print (box1.shape)
-        # print (len(box1[1]))
-        # print (confident_detections.scores)
+        pred_class = []
+        for i in confident_detections.pred_classes.__iter__():
+            pred_class.append(i.cpu().numpy())
         
         out = v.draw_instance_predictions(confident_detections.to("cpu"))
         cv2.imshow('frame', out.get_image()[:, :, ::-1])
 
-        # confident_detections.to("cpu")
-        # print (confident_detections.pred_boxes.to("cpu"))
-        # print (confident_detections.pred_boxes.numpy())
+        # # kitti format
+        for n_target in range (len(pred_class)):
+            frame = frame_id
+            target_id = n_target + 1            
+            type_id = pred_class[n_target]
+            output_pred_boxes = confident_detections.pred_boxes[n_target]
+            for i in output_pred_boxes.__iter__():
+                bbox = i.cpu().numpy()
+            
+            # print (bbox)
+            x1 = bbox[0]
+            y1 = bbox[1]
+            x2 = bbox[2]
+            y2 = bbox[3]
 
-        # kitti format
-        # for n_target in len(confident_detections.pred_classes)
-        #     target_id = n_target + 1
-        #     if confident_detections.pred_classes 
-            # results.append((frame_id, online_tlwhs, online_ids))
+            print (frame, target_id, type_id, x1, y1, x2, y2)
+            results.append((frame, target_id, type_id, x1, y1, x2, y2))
 
         frame_id = frame_id + 1
-
+    
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     else :
         
+        save_format = '{frame} {target_id} {type_id} 0 0 -10 {x1} {y1} {x2} {y2} -10 -10 -10 -1000 -1000 -1000 -10\n'
+        with open(kitti_filename, 'w') as f:
+            for frame, target_id, type_id, x1, y1, x2, y2 in results:
+                if type_id == 0:
+                    type_id = "person"
+                line = save_format.format(frame=frame, target_id=target_id, type_id=type_id, x1=x1, y1=y1, x2=x2, y2=y2)
+                f.write(line)
 
         video.release()
         cv2.destroyAllWindows()
